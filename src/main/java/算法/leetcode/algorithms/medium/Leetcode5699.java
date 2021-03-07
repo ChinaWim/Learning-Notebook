73af5cf2d3ef0d5b64d6843253cdabf2ef54b6be
wim <m969130721@163.com>
2021-03-07 23:59:01 +0800

    feat:记忆化搜索&最短路

--- src/main/java/算法/leetcode/algorithms/medium/Leetcode5699.java
+++ src/main/java/算法/leetcode/algorithms/medium/Leetcode5699.java
@@ -2,6 +2,7 @@ package 算法.leetcode.algorithms.medium;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.PriorityQueue;
 
 /**
  * [从第一个节点出发到最后一个节点的受限路径数]
@@ -22,11 +23,13 @@ import java.util.HashMap;
  * 3) 1 --> 3 --> 5
  *
  * 考点：最短路径算法 + 图的遍历方式
- * 1.dfs 2.bfs. 3.floyd 4.
+ * 1.dfs 2.bfs. 3.floyd 4.dijkstra 5.bellmanm(可以解决负权) 6.spfa(bellmanm的改进版本,可以解决负权)
  *
  */
 //todo
 public class Leetcode5699 {
+
+    static int mod = (int)1e9+7;
 
     public static void main(String[] args) {
         Leetcode5699 l = new Leetcode5699();
@@ -38,18 +41,18 @@ public class Leetcode5699 {
     }
 
     public int countRestrictedPaths(int n, int[][] edges) {
-        int infinity = 9999999;
-        int[][] array = new int[n + 1][n + 1];
+        int inf = 9999999;
+//        int[][] array = new int[n + 1][n + 1];
         int[][] dArray = new int[n + 1][n + 1];
 
         for(int i = 1; i <= n; i ++){
             for(int j = 1; j <= n; j++){
                 if(i == j){
-                    array[i][i] = 0;
+//                    array[i][i] = 0;
                     dArray[i][i] = 0;
                 }else {
-                    array[i][j] = infinity;
-                    dArray[i][j] = infinity;
+//                    array[i][j] = inf;
+                    dArray[i][j] = inf;
                 }
             }
         }
@@ -59,53 +62,95 @@ public class Leetcode5699 {
             int u1 = edge[0];
             int v1 = edge[1];
             int w1 = edge[2];
-            array[u1][v1] = w1;
-            array[v1][u1] = w1;
+//            array[u1][v1] = w1;
+//            array[v1][u1] = w1;
 
             dArray[u1][v1] = w1;
             dArray[v1][u1] = w1;
         }
+        int[][]disArray = new int[n+1][n+1];
+        int[][] disBox = new int[n + 1][n + 1];
+        //init dis
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <= n; j ++) {
+                disArray[i][j] = dArray[i][j];
+            }
+        }
+        this.dijkstra(n,inf,disArray,disBox,dArray);
 
-        this.floyd(n,infinity,array);
+//        this.floyd(n,inf,array);
         HashMap<Integer,Integer> indexContMap = new HashMap<>();
         for(int i = 1; i <= n; i ++){
-            indexContMap.put(i,array[i][n]);
+            indexContMap.put(i,disArray[i][n]);
         }
         int[]box = new int[n + 1];
         box[1] = 1;
-        return this.dfs(1,n,infinity,dArray,box,indexContMap);
+        int[] memo = new int[n + 1];
+        int result = this.dfs(1, n, inf, dArray, box, indexContMap,memo);
+
+        memo = null;
+        box = null;
+        disArray = null;
+        dArray = null;
+        indexContMap = null;
+        return result;
     }
 
     //O(n3)
-    private void floyd(int n,int infinity,int[][] array){
+    private void floyd(int n,int inf,int[][] array){
         for(int k = 1; k <= n ; k++ ){
             for(int i = 1; i <= n; i ++){
                 for(int j = 1; j <= n; j ++){
-                    if(array[i][j] > (array[i][k] + array[k][j]) && array[i][k] < infinity && array[k][j] < infinity ){
+                    if(array[i][j] > (array[i][k] + array[k][j]) && array[i][k] < inf && array[k][j] < inf ){
                         array[i][j] = (array[i][k] + array[k][j]);
                     }
                 }
             }
         }
     }
-    //O(n2)
-    private void dijkstra(){
 
+    //O(n2)
+    private void dijkstra(int n,int inf,int[][] dis,int[][] box,int[][] edge){
+        for(int k = 1; k <= n; k++){
+            for(int i = 1; i <= n - 1; i ++){
+                //先找出dis的当前最小值
+                int min = inf;
+                int u = 0;
+                for(int j = 1; j <=n ; j++){
+                    if(box[k][j] == 0 && dis[k][j] < min){
+                        min = dis[k][j];
+                        u = j;
+                    }
+                }
+                box[k][u] = 1;
+                for(int v = 1; v <= n; v++){
+                    if(edge[u][v] < inf && dis[k][v] > edge[u][v] + dis[k][u]){
+                        dis[k][v] = edge[u][v] + dis[k][u];
+                    }
+                }
+            }
+        }
     }
 
-    private int  dfs(int cur,int n,int infinity, int[][]dArray,int[] box,HashMap<Integer,Integer> indexContMap){
+
+    //记忆化搜索
+    private int  dfs(int cur,int n,int inf, int[][]dArray,int[] box,HashMap<Integer,Integer> indexContMap,int[] memo){
         int sum = 0;
         if(cur == n){
            return 1;
         }
+        if(memo[cur] != 0){
+            return memo[cur];
+        }
         for(int i = 1; i <= n; i ++){
-            if(box[i] == 0 && dArray[cur][i] != infinity && indexContMap.get(cur) > indexContMap.get(i)){
+            if(box[i] == 0 && dArray[cur][i] != inf && indexContMap.get(cur) > indexContMap.get(i)){
                 box[i] = 1;
-                sum += dfs(i,n,infinity,dArray,box,indexContMap);
+                sum = (sum + dfs(i,n,inf,dArray,box,indexContMap,memo)) % mod;
                 box[i] = 0;
             }
         }
-        return sum;
+        memo[cur] = sum % mod;
+        return memo[cur];
     }
 
 

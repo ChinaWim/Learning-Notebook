d82a1125fb9303b69cb5299d6576bf307d89b0e5
wim <m969130721@163.com>
2021-03-20 18:03:47 +0800

    daybyday

--- src/main/java/算法/leetcode/algorithms/medium/Leetcode11.java
+++ src/main/java/算法/leetcode/algorithms/medium/Leetcode11.java
@@ -54,7 +54,7 @@ public class Leetcode11 {
         int i = 0;
         int j = height.length - 1;
         int res = 0;
-        while(i >= 0  && i <= height.length - 1 && j>=0 && j <= height.length - 1){
+        while(i < j){
           res = Math.max(Math.min(height[i],height[j])*(j-i),res);
           if(height[i] < height[j]){
               i++;
